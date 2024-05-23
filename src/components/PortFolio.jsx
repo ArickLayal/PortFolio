0de3711b521de018a384b6e8fde72847800607be
priyanshtan <kash2001priyansh@gmail.com>
2024-05-23 10:31:12 +0530

    link disabled

--- src/components/PortFolio.jsx
+++ src/components/PortFolio.jsx
@@ -9,7 +9,7 @@ function PortFolio() {
       id: 1,
       logo: rent,
       name: "Car Rental System",
-      link: "https://github.com/priyanshtan/Car_Rental_System.git",
+      // link: "https://github.com/priyanshtan/Car_Rental_System.git",
       description:
         "Developed a mini project: a comprehensive car rental management system in Java, utilizing object-oriented programming principles for robust and maintainable code. The system covers customer management, vehicle tracking, and reservations."
     },
@@ -17,7 +17,7 @@ function PortFolio() {
       id: 2,
       logo: express,
       name: "College Management System",
-      link: "https://github.com/expressjs/express",
+      // link: "https://github.com/expressjs/express",
       description:
         "Currently working on a full-stack college management system using Node.js, Express, MongoDB, and Vue.js. The system includes student records, course registrations, attendance, and faculty management, along with a payment gateway integration. Node.js and Express handle the backend with RESTful APIs, while MongoDB securely stores academic data. The Vue.js frontend offers a responsive and interactive user interface. This project demonstrates expertise in modern web development, ensuring efficient data handling, a seamless user experience, and maintainable code."
     },
@@ -25,7 +25,7 @@ function PortFolio() {
       id: 3,
       logo: logo,
       name: "myStore",
-      link: "https://github.com/facebook/react",
+      // link: "https://github.com/facebook/react",
       description:
         "Developed a full-stack E-Commerce Website using Node.js, Express, MongoDB, and React. The project features user authentication, CRUD operations, and dynamic content rendering. Node.js and Express handle the backend, providing RESTful APIs, while MongoDB stores user data and application state. The React frontend offers a responsive, interactive user interface."
     }
@@ -40,7 +40,7 @@ function PortFolio() {
         <h1 className="text-3xl font-bold mb-5">Portfolio</h1>
         <span className="underline font-semibold">Featured Projects</span>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-3 my-7">
-          {cardItem.map(({ id, logo, name, description, link }) => (
+          {cardItem.map(({ id, logo, name, description }) => (
             <div
               className="md:w-[300px] border-[5px] rounded-lg shadow-lg p-4 flex flex-col justify-between cursor-pointer hover:scale-110 duration-300"
               key={id}
